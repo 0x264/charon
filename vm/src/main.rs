3e460c7c03408604c54daea5ec04987447056a91
0x264 <x-264@outlook.com>
2024-05-01 13:03:05 +0800

    add little endian byte reader

---
+++ vm/src/main.rs
@@ -0,0 +1,5 @@
+mod reader;
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ vm/src/reader.rs
@@ -0,0 +1,77 @@
+pub struct LEReader<'a> {
+    data: &'a [u8],
+    offset: usize
+}
+
+pub type Result<T> = std::result::Result<T, String>;
+
+impl LEReader<'_> {
+    pub fn new(data: &[u8]) -> LEReader {
+        LEReader {
+            data,
+            offset: 0
+        }
+    }
+    
+    pub fn next_u8(&mut self) -> Result<u8> {
+        match self.data.get(self.offset) {
+            None => Err(err_msg()),
+            Some(v) => {
+                self.offset += 1;
+                Ok(*v)
+            }
+        }
+    }
+    
+    pub fn next_u16(&mut self) -> Result<u16> {
+        if self.offset + 2 > self.data.len() {
+            Err(err_msg())
+        } else {
+            let res = unsafe {
+                read_u16_le(&self.data[self.offset..])
+            };
+            self.offset += 2;
+            Ok(res)
+        }
+    }
+
+    pub fn next_u32(&mut self) -> Result<u32> {
+        if self.offset + 4 > self.data.len() {
+            Err(err_msg())
+        } else {
+            let res = unsafe {
+                read_u32_le(&self.data[self.offset..])
+            };
+            self.offset += 4;
+            Ok(res)
+        }
+    }
+
+    pub fn next_u64(&mut self) -> Result<u64> {
+        if self.offset + 8 > self.data.len() {
+            Err(err_msg())
+        } else {
+            let res = unsafe {
+                read_u64_le(&self.data[self.offset..])
+            };
+            self.offset += 8;
+            Ok(res)
+        }
+    }
+}
+
+fn err_msg() -> String {
+    "end of input".to_owned()
+}
+
+unsafe fn read_u16_le(data: &[u8]) -> u16 {
+    u16::from_le(*(data.as_ptr() as *const u16))
+}
+
+unsafe fn read_u32_le(data: &[u8]) -> u32 {
+    u32::from_le(*(data.as_ptr() as *const u32))
+}
+
+unsafe fn read_u64_le(data: &[u8]) -> u64 {
+    u64::from_le(*(data.as_ptr() as *const u64))
+}
