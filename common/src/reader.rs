8aff872fcd21a74e0cb6427b47597a87f4ed0c08
0x264 <x-264@outlook.com>
2024-05-02 12:35:13 +0800

    fix: misaligned pointer in reader unsafe code

--- common/src/reader.rs
+++ common/src/reader.rs
@@ -79,14 +79,40 @@ fn err_msg() -> String {
     "end of input".to_owned()
 }
 
+// unsafe fn read_u16_le(data: &[u8]) -> u16 {
+//     u16::from_le(*(data.as_ptr() as *const u16))
+// }
+// 
+// unsafe fn read_u32_le(data: &[u8]) -> u32 {
+//     u32::from_le(*(data.as_ptr() as *const u32))
+// }
+// 
+// unsafe fn read_u64_le(data: &[u8]) -> u64 {
+//     u64::from_le(*(data.as_ptr() as *const u64))
+// }
+
 unsafe fn read_u16_le(data: &[u8]) -> u16 {
-    u16::from_le(*(data.as_ptr() as *const u16))
+    let b0 = *data.get_unchecked(0) as u16;
+    let b1 = *data.get_unchecked(1) as u16;
+    b1 << 8 | b0
 }
 
 unsafe fn read_u32_le(data: &[u8]) -> u32 {
-    u32::from_le(*(data.as_ptr() as *const u32))
+    let b0 = *data.get_unchecked(0) as u32;
+    let b1 = *data.get_unchecked(1) as u32;
+    let b2 = *data.get_unchecked(2) as u32;
+    let b3 = *data.get_unchecked(3) as u32;
+    b3 << 24 | b2 << 16 | b1 << 8 | b0
 }
 
 unsafe fn read_u64_le(data: &[u8]) -> u64 {
-    u64::from_le(*(data.as_ptr() as *const u64))
+    let b0 = *data.get_unchecked(0) as u64;
+    let b1 = *data.get_unchecked(1) as u64;
+    let b2 = *data.get_unchecked(2) as u64;
+    let b3 = *data.get_unchecked(3) as u64;
+    let b4 = *data.get_unchecked(4) as u64;
+    let b5 = *data.get_unchecked(5) as u64;
+    let b6 = *data.get_unchecked(6) as u64;
+    let b7 = *data.get_unchecked(7) as u64;
+    b7 << 56 | b6 << 48 | b5 << 40 | b4 << 32 | b3 << 24 | b2 << 16 | b1 << 8 | b0
 }
